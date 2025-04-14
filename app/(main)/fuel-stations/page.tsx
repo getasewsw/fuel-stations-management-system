38abd82a0c6808ca493faea40193b78f61dff46c
Lostinaa <Lostinaa@github.com>
2025-04-14 10:07:09 +0300

    re-routed fuel-station to (main)

--- app/(main)/fuel-stations/page.tsx
+++ app/(main)/fuel-stations/page.tsx
@@ -24,7 +24,7 @@ export default function FuelStationsPage() {
           <DialogTrigger asChild>
             <Button>
               <Plus className="mr-2 h-4 w-4" />
-              Add Station
+              Add Fuel Station
             </Button>
           </DialogTrigger>
           <DialogContent className="max-w-4xl">
